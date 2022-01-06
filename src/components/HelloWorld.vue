d17932a7f7a9eff430aae2b44b9b6fe3a7cd13ca
Shakil Ahmed <51516043+shakilahmed0369@users.noreply.github.com>
2022-01-06 11:51:11 +0600

    some change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,7 +43,7 @@ export default {
     }
   },
   mounted(){
-    axios.get('http://phplaravel-692081-2338552.cloudwaysapps.com/api/sliders', {dataType: "jsonp"}).then((res) => {
+    axios.get('http://phplaravel-692081-2338552.cloudwaysapps.com/api/sliders').then((res) => {
       console.log(res);
     });
   }
